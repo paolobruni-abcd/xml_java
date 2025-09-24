1f5b580231563997ff8c143d1f663911e3c3e664
paolobruni-abcd <paolo.bruni@itisarezzo.it>
2025-09-24 16:26:59 +0200

    uno
    uno uno

---
+++ xml_esempio 1 no attr.java
@@ -0,0 +1,111 @@
+package com.tutorialspoint.xml;
+
+import java.io.File;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXParserDemo {
+
+   public static void main(String[] args) {
+
+      try {
+         File inputFile = new File("input_xml1.xml");
+         SAXParserFactory factory = SAXParserFactory.newInstance();
+         SAXParser saxParser = factory.newSAXParser();
+         UserHandler userhandler = new UserHandler();
+         saxParser.parse(inputFile, userhandler);     
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }   
+}
+
+class UserHandler extends DefaultHandler {
+
+   boolean bFirstName = false;
+   boolean bLastName = false;
+   boolean bNickName = false;
+   boolean bMarks = false;
+
+  
+   public void startElement(
+      String uri, String localName, String qName, Attributes attributes)
+      throws SAXException {
+      
+	  if (qName.equalsIgnoreCase("firstname")) {
+         bFirstName = true;
+      } else if (qName.equalsIgnoreCase("lastname")) {
+         bLastName = true;
+      } else if (qName.equalsIgnoreCase("nickname")) {
+         bNickName = true;
+      }
+      else if (qName.equalsIgnoreCase("marks")) {
+         bMarks = true;
+      }
+   }
+
+   
+   public void endElement(String uri, 
+      String localName, String qName) throws SAXException {
+      
+      if (qName.equalsIgnoreCase("student")) {
+         System.out.println("End Element :" + qName);
+      }
+   }
+
+   
+   public void characters(char ch[], int start, int length) throws SAXException {
+
+      if (bFirstName) {
+         System.out.println("First Name: " + new String(ch, start, length));
+         bFirstName = false;
+      } else if (bLastName) {
+         System.out.println("Last Name: " + new String(ch, start, length));
+         bLastName = false;
+      } else if (bNickName) {
+         System.out.println("Nick Name: " + new String(ch, start, length));
+         bNickName = false;
+      } else if (bMarks) {
+         System.out.println("Marks: " + new String(ch, start, length));
+         bMarks = false;
+      }
+   }
+}
+
+
+/*
+<?xml version = "1.0"?>
+<class>
+   <student>
+      <firstname>dinkar</firstname>
+      <lastname>kad</lastname>
+      <nickname>dinkar</nickname>
+      <marks>85</marks>
+   </student>
+   
+   <student>
+      <firstname>Vaneet</firstname>
+      <lastname>Gupta</lastname>
+      <nickname>vinni</nickname>
+      <marks>95</marks>
+   </student>
+   
+   <student>
+      <firstname>jasvir</firstname>
+      <lastname>singn</lastname>
+      <nickname>jazz</nickname>
+      <marks>90</marks>
+   </student>
+   
+      <student>
+      <firstname>aaaaaa</firstname>
+      <lastname>bbbbbb</lastname>
+      <nickname>cccccc</nickname>
+      <marks>dddddd</marks>
+   </student>
+</class>
+*/
